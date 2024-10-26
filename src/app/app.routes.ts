fc08dbde86886cd2645c473f3638c570c1c041fc
scino11 <prod.scino@gmail.com>
2024-10-26 20:38:38 -0300

    Revert "arreglo"
    This reverts commit f5850b23c64001f3b9fb05efe672f218e6c193d7.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,18 @@
 import { Routes } from '@angular/router';
+import { verificarGuard } from './guard/verificar.guard';
+
 import { LoginComponent } from './pages/login/login.component';
-import { OrdenDeTrabajoComponent } from './components/gestion_ordenes/orden-de-trabajo/orden-contenedor/orden-de-trabajo.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { PlantillaComponent } from './components/plantillaOT/plantilla/plantilla.component';
-import { VerActivoComponent } from './components/gestion_infraestructura/activo/ver-activo/ver-activo.component';
-import { CrearActivoComponent } from './components/gestion_infraestructura/activo/crear-activo/crear-activo.component';
-import { verificarGuard } from './guard/verificar.guard';
-import { VistaOrdenOperarioComponent } from './components/vistaOperario/vista-orden-operario/vista-orden-operario.component';
-import { ModificarActivoComponent } from './components/gestion_infraestructura/activo/modificar-activo/modificar-activo.component';
-import { VerSectorComponent } from './components/gestion_infraestructura/sector/ver-sector/ver-sector.component';
-import { MenuPrincipalComponent } from './components/vistaAdmin/menu/menu-principal/menu-principal.component';
-import { DashboardComponent } from './components/vistaAdmin/menu/dashboard/dashboard.component';
+import { DashboardComponent } from './pages/vistaAdmin/menu/dashboard/dashboard.component';
+import { MenuPrincipalComponent } from './pages/vistaAdmin/menu/menu-principal/menu-principal.component';
+import { VerActivoComponent } from './pages/vistaAdmin/gestiones/gestion_infraestructura/activo/ver-activo/ver-activo.component';
+import { OrdenDeTrabajoComponent } from './pages/vistaAdmin/gestiones/gestion_ordenes/orden-de-trabajo/orden-contenedor/orden-de-trabajo.component';
+import { InfraestructuraComponent } from './pages/vistaAdmin/menu/infraestructura/infraestructura.component';
+import { OperariosComponent } from './pages/vistaAdmin/menu/operarios/operarios.component';
+import { VistaOrdenOperarioComponent } from './pages/vistaOperario/vista-orden-operario/vista-orden-operario.component';
+
+
 
 
 export const routes: Routes = [
@@ -23,14 +25,13 @@ export const routes: Routes = [
         children: [
             { path: 'dashboard', component: DashboardComponent },
             { path: 'activos', component: VerActivoComponent },
-            { path: 'ordenes', component: OrdenDeTrabajoComponent}
+            { path: 'ordenes', component: OrdenDeTrabajoComponent },
+            { path: 'infraestructura', component: InfraestructuraComponent },
+            { path: 'operarios', component: OperariosComponent }
         ]
     },
     
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'vistaUsuario', component:VistaOrdenOperarioComponent},
-    {path: 'crear/activos', component: CrearActivoComponent},
     {path: 'plantilla/:id', component: PlantillaComponent},
-    {path: 'modificarActivo/:id', component: ModificarActivoComponent},
-    {path: 'sectores', component:VerSectorComponent}
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
