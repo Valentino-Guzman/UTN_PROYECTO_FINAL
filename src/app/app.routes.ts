d8e5299df6f691733999218cc7f3132577fe7d6c
scino11 <prod.scino@gmail.com>
2024-10-27 08:26:11 -0300

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,17 +4,6 @@ import { verificarGuard } from './guard/verificar.guard';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { PlantillaComponent } from './components/plantillaOT/plantilla/plantilla.component';
-<<<<<<< HEAD
-import { VerActivoComponent } from './components/gestion_infraestructura/activo/ver-activo/ver-activo.component';
-import { CrearActivoComponent } from './components/gestion_infraestructura/activo/crear-activo/crear-activo.component';
-import { verificarGuard } from './guard/verificar.guard';
-import { VistaOrdenOperarioComponent } from './components/vistaOperario/vista-orden-operario/vista-orden-operario.component';
-import { ModificarActivoComponent } from './components/gestion_infraestructura/activo/modificar-activo/modificar-activo.component';
-import { VerSectorComponent } from './components/gestion_infraestructura/sector/ver-sector/ver-sector.component';
-import { MenuPrincipalComponent } from './components/vistaAdmin/menu/menu-principal/menu-principal.component';
-import { DashboardComponent } from './components/vistaAdmin/menu/dashboard/dashboard.component';
-import { CrearSectorComponent } from './components/gestion_infraestructura/sector/crear-sector/crear-sector.component';
-=======
 import { DashboardComponent } from './pages/vistaAdmin/menu/dashboard/dashboard.component';
 import { MenuPrincipalComponent } from './pages/vistaAdmin/menu/menu-principal/menu-principal.component';
 import { InfraestructuraComponent } from './pages/vistaAdmin/menu/infraestructura/infraestructura.component';
@@ -26,7 +15,6 @@ import { VerActivoComponent } from './components/gestiones/gestion_infraestructu
 import { SectorComponent } from './components/gestiones/gestion_ordenes/orden-de-trabajo/sector/sector.component';
 
 
->>>>>>> c8071c7d3cc190e136383fbcb89ef81aec7fff0b
 
 
 export const routes: Routes = [
@@ -53,11 +41,5 @@ export const routes: Routes = [
     
     { path: 'vistaUsuario', component:VistaOrdenOperarioComponent},
     {path: 'plantilla/:id', component: PlantillaComponent},
-<<<<<<< HEAD
-    {path: 'modificarActivo/:id', component: ModificarActivoComponent},
-    {path: 'sectores', component:VerSectorComponent},
-    {path: 'sectores', component:CrearSectorComponent}
-=======
     { path: '', redirectTo: '/home', pathMatch: 'full' },
->>>>>>> c8071c7d3cc190e136383fbcb89ef81aec7fff0b
 ];
