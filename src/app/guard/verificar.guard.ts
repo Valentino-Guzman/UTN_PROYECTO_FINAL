c99ad95963699391bcaa9bc5b5ebca8960c38b59
scino11 <prod.scino@gmail.com>
2024-11-14 21:30:15 -0300

    guard

--- src/app/guard/verificar.guard.ts
+++ src/app/guard/verificar.guard.ts
@@ -9,12 +9,18 @@ export const verificarGuard: CanActivateFn = (route, state) => {
 
   const userRole = cookieService.get('role');
   const userId = cookieService.get('operarioId');
+  const authToken = cookieService.get('authtoken'); 
 
-  if (userRole === 'admin') {
-    return true; 
-  } else {
+  if (userRole === 'admin' && authToken) {
+    return true;
+  } 
+  
+  if (userRole === 'user' && authToken) {
     console.log('Usuario no es admin.');
-    router.navigate([`vistaUsuario/${userId}`]);  
-    return false;  
-  }
+    router.navigate([`vistaUsuario/${userId}`]);
+    return false;
+  } 
+  
+  router.navigate(['/login']);
+  return false;
 };
